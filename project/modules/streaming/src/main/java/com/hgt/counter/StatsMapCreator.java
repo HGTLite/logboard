106be06796fb8d9b1bacdd9bfbb31e4c2ef9fc80
uptonking <jinyaoo86@gmail.com>
2016-11-26 19:10:37 +0800

    [add] stats map

---
+++ project/modules/streaming/src/main/java/com/hgt/counter/StatsMapCreator.java
@@ -0,0 +1,19 @@
+package com.hgt.counter;
+
+import java.util.HashMap;
+
+/**
+ * README:
+ * Created by Yao on 11/26/16.
+ * =============================================================================
+ * CHANGELOG:
+ */
+public class StatsMapCreator {
+
+    public HashMap<String, String> createStatsMap() {
+        HashMap<String, String> statsMap = new HashMap<>();
+
+        return statsMap;
+    }
+
+}
