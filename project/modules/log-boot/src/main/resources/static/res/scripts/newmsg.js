c7cfd683b03d71e859170e50f341179f37449c25
uptonking <jinyaoo86@gmail.com>
2016-12-04 23:58:31 +0800

    [add] index sockets

--- project/modules/log-boot/src/main/resources/static/res/scripts/newmsg.js
+++ project/modules/log-boot/src/main/resources/static/res/scripts/newmsg.js
@@ -3,6 +3,23 @@
  */
 function waitingForNewMsg(){
 
+    //var msgURLOrigin="http://localhost:8080"
+    var msgURLOrigin = "";
+    var totalLogCounts = $("#totalLogCounts");
+    var socket = new SockJS(msgURLOrigin + "/logging");
+
+    var stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+            //订阅的主题名为message
+            var topicLogCounts = "/topic/log-counts-streaming";
+            stompClient.subscribe(msgURLOrigin + topicLogCounts, function (data) {
+                var counts = data.body;
+                totalLogCounts.text(counts);
+            });
+        }
+    );
+
 }
 
 function renderDOMExpCurve(){
